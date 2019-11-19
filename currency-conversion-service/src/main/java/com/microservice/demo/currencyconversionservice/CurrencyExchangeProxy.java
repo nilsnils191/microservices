c7aa1ede34625088b4b619b0c97512a671af8a87
nilsnils191 <nilsnils191@gmail.com>
2019-11-19 23:25:33 +0530

    Using Feign and Ribbon client. Run second instance of CurrencyExchangeProxyService using vm arguments -Dserver.port=8001

--- currency-conversion-service/src/main/java/com/microservice/demo/currencyconversionservice/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/microservice/demo/currencyconversionservice/CurrencyExchangeProxy.java
@@ -1,12 +1,14 @@
 package com.microservice.demo.currencyconversionservice;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Component
-@FeignClient(name="currency-exchange-service",url="localhost:8000")
+@FeignClient(name="currency-exchange-service")
+@RibbonClient(name="currency-exchange-service")
 public interface CurrencyExchangeProxy {
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
